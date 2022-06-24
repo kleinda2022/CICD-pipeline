747dc4110b754a89a4c9d373033314626175492a
kysselle <nkysselle33@gmail.com>
2022-06-23 21:17:34 -0500

    my-variable-file

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIATWXEE5JSOJR7ZBJ4"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "us-east-1a"
+  }
